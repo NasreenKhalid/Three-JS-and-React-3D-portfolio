85cb6a15cdb6719c609d34d231909c4669dd5e30
Nasreen Shehzad <nasreen.1@live.com>
2024-04-02 14:05:06 +0300

    Initial Commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,32 @@
+import {Suspense} from 'react'
+import {Canvas} from '@react-three/fiber'
+import Loader from '../components/Loader'
+import Island from '../models/Island'
+
+const Home = () => {
+  return (
+    <section className='w-full h-screen relative'>
+      {/* <div className='absolute top-28 left-0 right-0 z-10 flex items-center justify-center'>
+        POPUP
+
+      </div> */}
+
+<Canvas className="w-full h-screen bg-transparent"
+camera={{ near: 0.1, far: 1000}}
+>
+
+<Suspense fallback={<Loader />}>
+<directionalLight />
+<ambientLight />
+<pointLight />
+<spotLight />
+<hemisphereLight />
+<Island />
+</Suspense>
+</Canvas>
+    </section>
+  )
+}
+
+export default Home
+
